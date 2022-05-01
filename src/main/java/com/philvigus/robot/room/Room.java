b354ae3c8709f9b0baef05b32b8ae7915775246d
PhilipVigus <philipvigus@yahoo.co.uk>
2022-05-01 10:09:48 +0100

    Implement isValidField

--- src/main/java/com/philvigus/robot/room/Room.java
+++ src/main/java/com/philvigus/robot/room/Room.java
@@ -16,4 +16,8 @@ public class Room {
   public int getLength() {
     return length;
   }
+
+  public boolean isValidField(int x, int y) {
+    return x >= 0 && x <= width && y >= 0 && y <= length;
+  }
 }
--- src/test/java/com/philvigus/robot/room/RoomTest.java
+++ src/test/java/com/philvigus/robot/room/RoomTest.java
@@ -2,7 +2,7 @@ package com.philvigus.robot.room;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class RoomTest {
   @Test
@@ -17,5 +17,40 @@ class RoomTest {
     Room room = new Room(3, 4);
 
     assertEquals(3, room.getWidth());
+  }
+
+  @Test
+  void isValidFieldShouldReturnTrueIfTheFieldIsValid() {
+    Room room = new Room(3, 4);
+
+    assertTrue(room.isValidField(1, 1));
+  }
+
+  @Test
+  void isValidFieldShouldReturnFalseIfXIsLessThanZero() {
+    Room room = new Room(3, 4);
+
+    assertFalse(room.isValidField(-1, 1));
+  }
+
+  @Test
+  void isValidFieldShouldReturnFalseIfYIsLessThanZero() {
+    Room room = new Room(3, 4);
+
+    assertFalse(room.isValidField(1, -1));
+  }
+
+  @Test
+  void isValidFieldShouldReturnFalseIfXIsMoreThanWidth() {
+    Room room = new Room(3, 4);
+
+    assertFalse(room.isValidField(4, 1));
+  }
+
+  @Test
+  void isValidFieldShouldReturnFalseIfYIsMoreThanWidth() {
+    Room room = new Room(3, 4);
+
+    assertFalse(room.isValidField(5, -1));
   }
 }

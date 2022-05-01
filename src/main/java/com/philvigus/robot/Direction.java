c0c9e055c7424415aac9163bb3d4cacd977a4a8e
PhilipVigus <philipvigus@yahoo.co.uk>
2022-05-01 09:15:51 +0100

    Add a position variable to each enum to help with turning

--- src/main/java/com/philvigus/robot/Direction.java
+++ src/main/java/com/philvigus/robot/Direction.java
@@ -1,5 +1,11 @@
 package com.philvigus.robot;
 
 public enum Direction {
-  N, E, S, W
+  N(0), E(1), S(2), W(3);
+
+  private final Integer position;
+
+  Direction(final Integer position) {
+    this.position = position;
+  }
 }

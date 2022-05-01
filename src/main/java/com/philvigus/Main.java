f9fdba408facd6ad884eae1e9b6b692b36e1e594
PhilipVigus <philipvigus@yahoo.co.uk>
2022-05-01 11:44:08 +0100

    Validate the position against the room size

--- src/main/java/com/philvigus/Main.java
+++ src/main/java/com/philvigus/Main.java
@@ -3,13 +3,15 @@ package com.philvigus;
 import com.philvigus.robot.ui.ConsoleUi;
 import com.philvigus.robot.ui.PositionAndDirectionReader;
 import com.philvigus.robot.ui.RoomSizeReader;
+import com.philvigus.robot.world.RoomImpl;
 
 public class Main {
   public static void main(String[] args) {
     ConsoleUi ui = new ConsoleUi(System.in, System.out);
     RoomSizeReader reader = new RoomSizeReader(ui);
 
-    PositionAndDirectionReader positionAndDirectionReader = new PositionAndDirectionReader(ui);
+    PositionAndDirectionReader positionAndDirectionReader =
+        new PositionAndDirectionReader(ui, new RoomImpl(10, 10));
     String prompt = positionAndDirectionReader.readInput();
     ui.displayMessage(prompt);
   }
--- src/main/java/com/philvigus/robot/ui/PositionAndDirectionReader.java
+++ src/main/java/com/philvigus/robot/ui/PositionAndDirectionReader.java
@@ -1,15 +1,20 @@
 package com.philvigus.robot.ui;
 
+import com.philvigus.robot.world.Room;
+
 import java.util.regex.Matcher;
 
 public class PositionAndDirectionReader extends ValidatedStringReader {
+  private final Room room;
 
-  public PositionAndDirectionReader(Ui ui) {
+  public PositionAndDirectionReader(Ui ui, Room room) {
     super(
         ui,
         "Please enter the position and direction",
         "^\\d+ \\d+ [NSEW]$",
         "The input must be specified in the form of two positive numbers separated by a space, followed by one of N, S, E or W");
+
+    this.room = room;
   }
 
   @Override
@@ -20,6 +25,16 @@ public class PositionAndDirectionReader extends ValidatedStringReader {
     // Does it match the pattern?
     if (!matcher.matches()) {
       ui.displayMessage(error);
+      return readInput();
+    }
+
+    final String[] data = input.split(" ");
+    final int x = Integer.parseInt(data[0]);
+    final int y = Integer.parseInt(data[1]);
+
+    // Is the start position valid for the room size?
+    if (!room.isValidField(x, y)) {
+      ui.displayMessage("The position must be within the room");
       return readInput();
     }
 
